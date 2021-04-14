051510de913748e2956fb4b704a65e879748d945
Vivek Madarapu <vivek.madarapu.800@k12.friscoisd.org>
2021-04-14 12:09:52 -0500

    finished map loading functionality

--- Blackout/Blackout/Tile.cs
+++ Blackout/Blackout/Tile.cs
@@ -12,7 +12,7 @@ namespace Blackout
     {
         public static String DEFAULT_TILE = "bt1";
         public static int TICKS_PER_FRAME = 4;
-        public static int TILE_SIZE = 32;
+        public static int TILE_SIZE = 64;
         Texture2D tileTexture;
         int animationFrames;
         double x, y;
